5e8afe3f3e7acd5d9923ef97f07570eac37de39b
Wagner Fontes <wsf@ic.ufal.br>
2018-12-19 10:12:27 -0300

    Update PogWave.cpp

--- PogWave.cpp
+++ PogWave.cpp
@@ -2,61 +2,55 @@
 #include <GL/glut.h>
 #include <vector>
 #include <iostream>
-#include <stdlib.h>     /* srand, rand */
-#include <time.h>       /* time */
+#include <stdlib.h>
+#include <time.h>
 
 using namespace std;
 
-const int width = 720, height = 250;
+
 const float frameTime = 1000.0f / 60.0f, v = 12;
 
-struct square {
+struct sequence {
    float x, y, edgeSize, R, G, B;
    int direction = 1;
-    bool canMove = false;
+   bool canMove = false;
 };
 
-vector<square> blocks;
+vector<sequence> points;
 
-void drawSquare(float x, float y) {
+void drawsequence(float x, float y) {
 
-	glColor3ub(255,255,255);
+	 glColor3ub(255,255,255);
    glBegin(GL_POINTS);
       glVertex2f(x,  y);
    glEnd();
+
 }
 
-square createSquare() {
-   square newBlock = square();
-   newBlock.edgeSize = 40;
-   newBlock.x = 2 + blocks.size() * 5;
+sequence createsequence() {
+   sequence newBlock = sequence();
+   newBlock.x = 2 + points.size() * 10;
    newBlock.y = 0;
-   double color[3] = {};
-   srand((unsigned int) time(NULL));
-   newBlock.R = (float) color[0];
-   newBlock.G = (float) color[1];
-   newBlock.B = (float) color[2];
-
    return newBlock;
 }
 
 void scheduleUpdate(int useless) {
    glutTimerFunc((unsigned int) frameTime, scheduleUpdate, 1);
 
-   for (int i = 0; i < (int) blocks.size(); i++) {
-       if ((blocks[i].canMove && (i == 0 || blocks[i - 1].y >= 30)) || blocks[i].y != 0) {
-           blocks[i].y += v * blocks[i].direction;
+   for (int i = 0; i < (int) points.size(); i++) {
+       if ((points[i].canMove && (i == 0 || points[i - 1].y >= 30)) || points[i].y != 0) {
+           points[i].y += v * points[i].direction;
 
-           if (blocks[i].y >= height - blocks[i].edgeSize) {
-               blocks[i].direction = -1;
-               blocks[i].y = height - blocks[i].edgeSize;
-               blocks[i].canMove = i == 0 ? false : blocks[i - 1].canMove;
-           } else if (blocks[i].y <= 0) {
-               blocks[i].direction = 1;
-               blocks[i].y = 0;
+           if (points[i].y >= 600 - points[i].edgeSize) {
+               points[i].direction = -1;
+               points[i].y = 600 - points[i].edgeSize;
+               points[i].canMove = i == 0 ? false : points[i - 1].canMove;
+
+           } else if (points[i].y <= 0) {
+               points[i].direction = 1;
+               points[i].y = 0;
            }
 
-           cout << "   x: " << blocks[i].x << ", y: " << blocks[i].y << endl;
        }
    }
 
@@ -64,47 +58,45 @@ void scheduleUpdate(int useless) {
 }
 
 void keyboardHandler(unsigned char key, int x, int y) {
-    switch (key) {
-        case ' ':
-            for (int i = 0; i < blocks.size(); i++) {
-                blocks[i].canMove = true;
+    if(key == ' '){
+            for (int i = 0; i < points.size(); i++) {
+                points[i].canMove = true;
             }
-            break;
-        default:
-            break;
     }
 }
 
 void display() {
-   glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set background color to black and opaque
-   glClear(GL_COLOR_BUFFER_BIT);         // Clear the color buffer
+   glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+   glClear(GL_COLOR_BUFFER_BIT);
 
-   for (int i = 0; i < (int) blocks.size(); i++) {
-      drawSquare(blocks[i].x, blocks[i].y);
+   for (int j = 0; j < 420 ; j++) {
+     for (int i = 0; i < (int) points.size(); i++) {
+        drawsequence(points[i].x, points[i].y + j);
+     }
    }
 
-   glFlush();  // Render now
+   glFlush();
 }
 
 void init() {
-  gluOrtho2D(0, width, 0, height);
+  gluOrtho2D(0, 800, 0, 600);
 }
 
 int main(int argc, char** argv) {
 
-    for (int i = 0; i < 150; i++) {
-        blocks.push_back(createSquare());
+    for (int i = 0; i < 200; i++) {
+        points.push_back(createsequence());
     }
 
-   glutInit(&argc, argv);                 // Initialize GLUT
+   glutInit(&argc, argv);
       glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-      glutInitWindowSize(width, height);   // Set the window's initial width & height
-      glutInitWindowPosition(50, 50); // Position the window's initial top-left corner
-      glutCreateWindow("OpenGL Project - Wave"); // Create a window with the given title
+      glutInitWindowSize(800, 600);
+      glutInitWindowPosition(50, 50);
+      glutCreateWindow("Olha onda, olha onda");
       init();
-   glutDisplayFunc(display); // Register display callback handler for window re-paint
-   glutKeyboardFunc(keyboardHandler); // Register display callback handler for window re-paint
+   glutDisplayFunc(display);
+   glutKeyboardFunc(keyboardHandler);
    glutTimerFunc((unsigned int) frameTime, scheduleUpdate, 1);
-   glutMainLoop();           // Enter the infinitely event-processing loop
+   glutMainLoop();
    return 0;
 }
